4e768b625b5acb365dbd110a7417238ad57a8bdc
Aishwarya Tiwari <Atiwari0929@conestogac.on.ca>
2019-12-11 17:07:16 -0500

    Shopping cart implementation

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{product.title}} -
+        {{product.price | currency}} -
+        {{product.quantity}}
+      </li>
+    </ul>
+    <p>Total:{{total | currency}}</p>
+    <button @click="checkout">Checkout</button>
+    <p v-if="statusMessage">{{statusMessage}}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters,mapActions } from "vuex";
+export default {
+  methods: {
+      ...mapActions({
+        checkout:'checkout'  
+      })
+  },
+  computed: {
+    ...mapGetters({
+      products: "cartProducts",
+      total: "cartTotal"
+    }),
+    ...mapState({
+        statusMessage:state=>state.checkoutStatus,
+    })
+  }
+};
+</script>
+
+<style>
+</style>
